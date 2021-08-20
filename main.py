afbf8cd1b39b651733e93a21cf3fc6496c916fc8
floh0 <florian.dasilva123@gmail.com>
2021-08-20 14:58:04 +0200

    can now pass base_url through plugin settings

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import subprocess
 
 class GoToRedmineCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		BASE_URL = "FILL_HERE"
-		selection = self.view.sel()
-		selectedTexts = [self.view.substr(self.view.word(region)) for region in selection]
-		redmineIds = { text for text in selectedTexts if re.match(r"^\d+$", text) }
-		for text in redmineIds:
-			url = BASE_URL + "/issues/{}".format(text)
-			subprocess.Popen("start {}".format(url), shell=True)
+		settings = sublime.load_settings("Go to redmine.sublime-settings")
+		baseUrl = settings.get("base_url")
+		if baseUrl is None:
+			sublime.error_message("[Error] No value found for base_url. Please go to Preferences > Package Settings > Go to redmine > Settings.")
+		else:
+			selection = self.view.sel()
+			selectedTexts = [self.view.substr(self.view.word(region)) for region in selection]
+			redmineIds = { text for text in selectedTexts if re.match(r"^\d+$", text) }
+			for text in redmineIds:
+				url = baseUrl + "/issues/{}".format(text)
+				subprocess.Popen("start {}".format(url), shell=True)
